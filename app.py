b9a3a82bc26b35496d3336c5a388051082b4e4c3
Bharatesha_NS <bharatesh143ns@gmail.com>
2024-01-15 13:57:45 +0530

    This commit includes web application files

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-import pickle
-from flask import Flask,request,app,jsonify,url_for,render_template
+# Import necessary libraries
+from flask import Flask, request, render_template
 import numpy as np
-import pandas as pd
+import pickle
 
-app =Flask(__name__)
+app = Flask(__name__)
 model= pickle.load(open('RF_model.pkl','rb'))
 
 @app.route('/')
-
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods= ['POST'])
-
+@app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
          # Extract input values from the form
         gender = request.form['gender']
-        gender = request.form['gender']
+        dependents = request.form['dependents']
         married = request.form['married']
         education = request.form['education']
         self_employed = request.form['self_employed']
@@ -33,16 +31,26 @@ def predict():
         education = 1 if education == 'Graduate' else 0
         self_employed = 1 if self_employed == 'Yes' else 0
         credit_card = 1 if credit_card == 'Yes' else 0
+        dependents_mapping = {"1": 1, "2": 2, "3+": 3}  # Define the mapping
+        # Convert dependents to an integer using the mapping
+        dependents = dependents_mapping.get(dependents, 0)
 
         # Make a prediction using the loaded model
-        features = np.array([[gender, married, education, self_employed,
+        features = np.array([[gender, married,dependents, education, self_employed,
                               applicant_income, coapplicant_income,
                               loan_amount, loan_amount_term, credit_card]])
         prediction = model.predict(features)
-# Interpret the prediction result
-        result = "Yes you can Apply for Loan" if prediction[0] == 1 else "No you can't Apply for Loan" 
+        
+        # Interpret the prediction result
+        result = "Yes you can Apply" if prediction[0] == 1 else "No you can't Apply"
 
-        return render_template('index.html', result=result)
+    # Render the result template with the original form values
+    return render_template('results.html', form_values=request.form, result=result)
+
+# New route for the home page after prediction
+@app.route('/home')
+def go_home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
